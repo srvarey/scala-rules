d4566ffc12b3828255e4211f5b9f0d113a5292d9
Andrew.Foggin <Andrew.Foggin@dd424217-7b3a-0410-8912-73af84f046af>
2007-10-27 04:08:53 +0000

    removed readif(...) from Parser because elem filter (...) is simply better!  Added choice(seq).  Changed some vals to defs so they can be overridden.

--- trunk/scala-rules/src/net/foggin/rules/Parser.scala
+++ trunk/scala-rules/src/net/foggin/rules/Parser.scala
@@ -15,41 +15,40 @@ class ArrayInput[A](val array : Array[A], val index : Int) extends Input[A, Arra
 }
 
 
-
-/** 
+/**
  * A Parser is used to define rules that operate on sequential input
  */
 trait Parser[A] extends Rules {
   type Context <: Input[A, Context]
 
-  /** Succeeds with the first element of the input unless input is empty.
-   */
+  /** Succeeds with the first element of the input unless input is empty. */
   val item = createRule { input => input.next }
-  
-  implicit def elem(a : A) = for (x <- item if x == a) yield x
-  
-  final def readSeq[C <% Seq[A]](seq : C) : Rule[C] = 
-    seq.map(elem(_)).reduceLeft[Rule[A]](_ -~ _) ^^ (any => seq)
-     
-  def readIf(condition : A => Boolean) = for (x <- item if condition(x)) yield x
+
+  implicit def elem(a : A) = item filter (_ == a)
+
+  def readSeq[C <% Seq[A]](seq : C) : Rule[C] =
+      seq.map(elem(_)).reduceLeft[Rule[A]](_ -~ _) ^^ (any => seq)
+
+  def choice[C <% Seq[A]](seq : C) : Rule[A] =
+      seq.map(elem(_)).reduceLeft[Rule[A]](_ | _)
+
 }
 
-/** 
+   
+/**
  * A Scanner is a parser for character input.
  */
 trait Scanner extends Parser[Char] {
-     
   implicit def readString(string : String) : Rule[String] = readSeq(string)
-  
   implicit def stringToInput(string : String) : ArrayInput[Char] = new ArrayInput[Char](string.toArray)
-  
-  def literal(seq : Seq[Char]) = seq.mkString("")
-    
-  val letter = readIf(_ isLetter)
-  val digit = readIf(_ isDigit)
-  val whitespace = readIf(_ isWhitespace)*
-  val newline = "\r\n" | "\n" | "\r"
-  
+
+  def literal(seq : Seq[Any]) = seq.mkString("")
+
+  def letter = item filter (_ isLetter)
+  def digit = item filter (_ isDigit)
+  def whitespace = item filter (_ isWhitespace)*
+  def newline = "\r\n" | "\n" | "\r"
+
   def trim[A](rule : Rule[A]) = whitespace -~ rule ~- whitespace
 }
 
