c6b34066ac9b6be899086747b6d6aad2199fb9f3
Andrew.Foggin <Andrew.Foggin@dd424217-7b3a-0410-8912-73af84f046af>
2007-10-30 11:18:21 +0000

    View one type of parser as another.  More Scala scanner rules.

--- scala-rules/src/net/foggin/rules/ScalaScanner.scala
+++ scala-rules/src/net/foggin/rules/ScalaScanner.scala
@@ -1,56 +1,130 @@
 package net.foggin.rules
 
+import Character._
+
 abstract class ScalaScanner extends Scanner {
 
   val decimalDigit = choice("0123456789") ^^ (_ - 48)
   val octalDigit = decimalDigit.filter(_ < 8)
   val hexDigit = decimalDigit | choice("ABCDEF") ^^ (_ - 55) | choice("abcdef") ^^ (_ - 87)
 
-  val unicodeEscape = "\\u" -~ hexDigit >> nextHexDigit >> nextHexDigit >> nextHexDigit ^^ { _.asInstanceOf[Char] }
-  val octalEscape = '\\' -~ octalDigit >> nextOctalDigit >> nextOctalDigit ^^ { _.asInstanceOf[Char] }
-
-  def nextOctalDigit(n : Int) = octalDigit ^^ (n * 8 + _)
-  def nextHexDigit(n : Int) = hexDigit ^^ (n * 16 + _)
+  val unicodeEscape = "\\u" -~ hexDigit >> hex >> hex >> hex ^^ { _.asInstanceOf[Char] }
+  val octalEscape = '\\' -~ octalDigit >> oct >> oct ^^ { _.asInstanceOf[Char] }
+ 
+  def dec(n : Int) = decimalDigit ^^ (n * 10 + _)
+  def oct(n : Int) = octalDigit ^^ (n * 8 + _)
+  def hex(n : Int) = hexDigit ^^ (n * 16 + _)
+ 
+  def decN(n : Int) : Rule[Int] = dec(n) >> decN | success(n)
+  def octN(n : Int) : Rule[Int] = oct(n) >> octN | success(n)
+  def hexN(n : Int) : Rule[Int] = hex(n) >> hexN | success(n)
 
   val anyChar = unicodeEscape | octalEscape | item
-  val idStart = anyChar.filter(Character isJavaIdentifierStart)
-  val idPart = anyChar.filter(Character isJavaIdentifierPart)
-  val opChar = (!idPart | choice(" \t\n\r(){}[]")) -~ anyChar
+  
+  def unicode(category : Int) = anyChar filter (getType(_) == category)
+  
+  val upper = anyChar filter isUpperCase
+  val lower = anyChar filter isLowerCase
+  val opChar = !choice(".()[]{}") -~ choice("!#%&*+-/:<=>?@\\^|~") | unicode(MATH_SYMBOL) | unicode(OTHER_SYMBOL)
+  
+  val op = opChar+
+  val idrest = !('_' ~ opChar) -~ (letter | digit *) ~ ('_' ~++ op ?) ^^ { case a ~ Some(b) => a ::: '_' :: b case a ~ None => a }
+  val varid = lower ~++ idrest
+  val plainid = upper ~++ idrest | varid | op
+  val id = (plainid | '`' -~ (anyChar+) ~- '`') ^^ literal
+    
+  val zero = elem('0')
+  //val digit = choice("0123456789")
+  val decimalNumeral = "0" | !zero -~ (digit+) ^^ literal
+  val hexNumeral = "0x" -~ (choice("0123456789abcdefABCDEF")+) ^^ literal
+  val octalNumeral = '0' -~ (choice("01234567")+) ^^ literal
+  val integerLiteral = (decimalNumeral | hexNumeral | octalNumeral) ~- (choice("Ll")?)
+  val floatingPointLiteral = 
+    (digit+) ~- '.' ~ (digit*) ~ (exponentPart?) ~ (floatType?) ^^^ "123" |
+    '.' ~ (digit+) ~ (exponentPart?) ~ (floatType?) ^^^ "123" |
+    (digit+) ~ exponentPart ~ (floatType?) ^^^ "123" |
+    (digit+) ~ (exponentPart?) ~ floatType ^^^ "123"
+  val exponentPart = choice("eE") -~ (choice("+-")?) ~ (digit+)
+  val floatType = choice("fFdD")
 
-  val name = idStart ~++ (idPart*) ^^ literal
+  val booleanLiteral = "true" | "false"
+
+  val charEscapeSeq = 
+      "\\b" ^^^ '\b' | 
+      "\\t" ^^^ '\t' | 
+      "\\n" ^^^ '\n' | 
+      "\\f" ^^^ '\f' | 
+      "\\r" ^^^ '\r' | 
+      "\\\"" ^^^ '\"' | 
+      "\\\'" ^^^ '\'' | 
+      "\\\\" ^^^ '\\'
+
+  val characterLiteral = '\'' -~ (charEscapeSeq | !choice("\'\n\r\\") -~ anyChar) ~- '\''
+  val stringLiteral = 
+      '\"' -~ ((charEscapeSeq | !choice("\"\n\r\\") -~ anyChar)*) ~- '\"' ^^ literal |
+      "\"\"\"" -~ ((!"\"\"\"" -~ anyChar)*) ~- "\"\"\"" ^^ literal
+  val symbolLiteral = '\'' ~ plainid
+  
+  val lineComment = "//" -~ ((!newline -~ item)*)
+  
+  val semi = (";" | newline) ~- (newline*)
+   
+  //val name = idStart ~++ (idPart*) ^^ literal
   val operator = (opChar+) ^^ literal
-  val special = "(" | ")" | "{" | "}"
-  val keyword = ("package" | "import" | "object") ~- (!idPart)
+  val special = choice(".()[]{}")
+  val keyword = ("package" | "import" | "object" | "extends") ~- !(anyChar filter isJavaIdentifierPart)
   val other = (item filter { ch => !(ch isWhitespace) } +) ^^ literal
-  val stringLiteral = '"' -~ (item filter (_ != '"')*) ~- '"' ^^ literal
+  //val stringLiteral = '"' -~ (item filter (_ != '"')*) ~- '"' ^^ literal
 
-  val token = (whitespace?) -~ (special | keyword | stringLiteral | comment | operator | name | other)
-  val tokens = (token*) ~- whitespace
-
-  val line = ((!newline -~ item)*) ^^ literal
-  val lines = line ~++ (newline -~ line *)
-
+  // note multi-line comments can nest
   lazy val comment : Rule[String] = "/*" -~ ((!"*/") -~ (comment | anyChar)).* ~- "*/" ^^ literal
 
 }
 
-object TestScalaScanner extends ScalaScanner with Application {
-  type Context = ArrayInput[Char]
+abstract class IncrementalScalaScanner extends ScalaScanner with IncrementalScanner {
+  val token = memo("token", (whitespace?) -~ (special | keyword | stringLiteral | comment | id | other))
+  val tokens = view(token) _
 
-  println(unicodeEscape("\\u0030"))
-  println(octalEscape("\\060"))
+  val line = memo("line", newline ^^^ "" | (!newline -~ item +) ~- (newline?) ^^ literal)
+  val lines = view(line) _
+}
 
-  val input = """
+object TestScalaScanner extends IncrementalScalaScanner with Application {
+  var input = new EditableInput[Char]
+
+  //println(unicodeEscape("\\u0030"))
+  //println(octalEscape("\\060"))
+
+  def printTokens() {
+    println; println("Tokens: ")
+    println(tokens(input).mkString("\"", "\", \"", "\""))
+  }
+
+  def printLines() {
+    println; println("Lines: ")
+    println(lines(input).mkString("\n"))
+  }
+
+  // set up initial text
+  input = input.edit(0, 0, """
     package a.b.c
-
+    
     /** my comment */
     object Hello extends Application {
       println("Hello World!")
     }
-    """
+    """)
 
-  println(tokens(input))
-  println(lines(input))
-}
+  printTokens()
+  printLines()
 
- 
+ // insert something
+ input = input.edit(19, 0, """
+   class Dummy {
+     val answer = 42
+   }
+   """)
+
+  printTokens()
+  printLines()
+} 
