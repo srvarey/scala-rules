87c398bfd958a7ef9831a8e9cfc89cec09cd6464
Andrew.Foggin <Andrew.Foggin@dd424217-7b3a-0410-8912-73af84f046af>
2007-11-19 05:09:24 +0000

    Some improvements.  Extracted example to its own package.

---
+++ scala-rules/src/net/foggin/rules/example/ArithmeticEvaluator.scala
@@ -0,0 +1,31 @@
+package net.foggin.rules.example
+
+trait ArithmeticEvaluator extends IncrementalScanner {
+  lazy val expr : Rule[Int] = term ~*~ (op('+', _ + _) | op('-', _ - _))
+  lazy val term : Rule[Int] = factor ~*~ (op('*', _ * _) | op('/', _ / _))
+  lazy val factor : Rule[Int] = memo("factor", trim(number | '(' -~ expr ~- ')'))
+  lazy val number = (('0' to '9')+) ^^ toString ^^ (_ toInt)
+  
+  private def op(r : Rule[Any], f : (Int, Int) => Int) = r -^ f
+
+  def evaluate = expect(expr ~- !item | exception("Invalid expression"))
+}
+  
+object ExampleUsage extends ArithmeticEvaluator with Application {
+  val document = new EditableDocument[Char]
+  val input = document.first
+  
+  document.debug = true
+  
+  // set up initial text and evaluate
+  document.edit(0, 0, "7 + 5 * (5+ 6 / 2 - 1)")
+  println(evaluate(input))
+  
+   // change to "7 + (5 + 1) * (5+ 6 / 2 - 1)"
+  document.edit(4, 1, "(5 + 1)")
+  println(evaluate(input))
+  
+   // change to "(5 + 1) * (5+ 6 / 2 - 1)"
+  document.edit(0, 4, "")
+  println(evaluate(input))
+}
