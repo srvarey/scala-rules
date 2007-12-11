5eb40016d1add2e11569c56e002ca00e9f02c732
Andrew.Foggin <Andrew.Foggin@dd424217-7b3a-0410-8912-73af84f046af>
2007-12-11 13:40:38 +0000

    An alternative approach to incremental highlighting in plug-in

--- rule-plugin/src/net/foggin/rules/plugin/ScalaEditor.scala
+++ rule-plugin/src/net/foggin/rules/plugin/ScalaEditor.scala
@@ -3,18 +3,24 @@ package net.foggin.rules.plugin
 import org.eclipse.jface.text.source._
 import org.eclipse.ui.editors.text._
 
+import org.eclipse.swt.graphics.Color
+import org.eclipse.swt.graphics.RGB
+import org.eclipse.swt.widgets.Display
+
 class ScalaEditor extends TextEditor {
 
-  val parser = new PluginParser
+  private val colours = new _root_.scala.collection.mutable.HashMap[RGB, Color]
+
+  def colour(rgb: RGB) = colours.getOrElseUpdate(rgb, new Color(Display.getCurrent(), rgb))
   
   setSourceViewerConfiguration(new SourceViewerConfiguration() {
-    override def getPresentationReconciler(sourceViewer : ISourceViewer) = new ScalaPresentationReconciler(parser)
+    override def getPresentationReconciler(sourceViewer : ISourceViewer) = new ScalaPresentationReconciler(ScalaEditor.this)
   })
   
   setDocumentProvider(new FileDocumentProvider())
   
   override def dispose() {
-    parser.dispose()
+    for (colour <- colours.values) colour.dispose()
     super.dispose()
   }
 }
--- rule-plugin/src/net/foggin/rules/plugin/ScalaPresentationReconciler.scala
+++ rule-plugin/src/net/foggin/rules/plugin/ScalaPresentationReconciler.scala
@@ -1,14 +1,80 @@
 package net.foggin.rules.plugin
 
-import org.eclipse.swt.custom.StyleRange;
-
 import org.eclipse.jface.text._
 import org.eclipse.jface.text.presentation._
+import org.eclipse.jface.text.source._
 
-class ScalaPresentationReconciler(parser : PluginParser) extends IPresentationReconciler with IPresentationReconcilerExtension {
+import org.eclipse.swt.SWT
+import org.eclipse.swt.custom.StyleRange
+import org.eclipse.swt.graphics.RGB
 
+class ScalaPresentationReconciler(editor : ScalaEditor) extends IPresentationReconciler with IPresentationReconcilerExtension {
+  
+  val DEFAULT = new TextAttribute(editor.colour(new RGB(0, 0, 0)))
+  val KEYWORD = new TextAttribute(editor.colour(new RGB(128, 128, 128)), null, SWT.BOLD)
+  val LITERAL = new TextAttribute(editor.colour(new RGB(0, 128, 0)))
+  val COMMENT = new TextAttribute(editor.colour(new RGB(128, 128, 255)), null, SWT.ITALIC)
+
+  val parser = new scala.ScalaParser[ScalaDocumentInput] { }
+  
+  def damage(from : Int, to : Int) {
+    if (damageStart < 0 || from < damageStart) damageStart = from
+    if (to > damageEnd) damageEnd = to
+  }
+  
+  var damageStart = -1
+  var damageEnd = -1
+
+  class ScalaDocument extends EditableDocument[Char, ScalaDocumentInput] {
+    val first = new ScalaDocumentInput
+  }
+  
+  class ScalaDocumentInput extends IncrementalInput[Char, ScalaDocumentInput] {
+    def element = new ScalaDocumentInput
+
+    override protected def onSuccess[T](key : AnyRef,  result : Success[T, ScalaDocumentInput]) {
+      //println(key + " -> " + result)
+      key match {
+        case ("keyword", _) => keyword(index, result.rest.index)
+        case ("literal", _) => literal(index, result.rest.index)
+        case ("comment", _) => comment(index, result.rest.index)
+        case _ => // do nothing
+      }
+    }
+    
+    override def cleanResults(pos : Int) = {
+      map foreach {
+        case ((key : AnyRef, _), Success(_, elem)) if elem.index >= pos => remove(key, index, elem.index)
+        case _ => 
+      }
+      super.cleanResults(pos)
+    }
+    
+    private def remove(key : AnyRef, from : Int, to : Int) = key match {
+      case "keyword" | "literal" | "comment" => damage(from, to)
+      case _ =>
+    }
+    
+    override def toString = "@" + index
+  }
+
+  def keyword(start : Int, end : Int) = applyStyle(start, end, KEYWORD)
+  def literal(start : Int, end : Int) = applyStyle(start, end, LITERAL)
+  def comment(start : Int, end : Int) = applyStyle(start, end, COMMENT)
+  def default(start : Int, end : Int) = applyStyle(start, end, DEFAULT)
+
+  def applyStyle(start : Int, end : Int, attribute : TextAttribute) {
+    val style= attribute.getStyle
+    val fontStyle = attribute.getStyle & (SWT.ITALIC | SWT.BOLD | SWT.NORMAL)
+    val styleRange = new StyleRange(start, end - start, attribute.getForeground, attribute.getBackground, fontStyle)
+    styleRange.strikeout = (attribute.getStyle & TextAttribute.STRIKETHROUGH) != 0
+    styleRange.underline = (attribute.getStyle & TextAttribute.UNDERLINE) != 0
+    styleRange.font = attribute.getFont
+    fViewer.getTextWidget.setStyleRange(styleRange)
+  }
+  
    private var fViewer : ITextViewer = null
-   private var scalaDocument : DefaultDocument = null
+   private var scalaDocument : ScalaDocument = null
 
    private val inputListener = new ITextInputListener {
     def inputDocumentAboutToBeChanged(oldDocument : IDocument, newDocument : IDocument) {
@@ -23,6 +89,7 @@ class ScalaPresentationReconciler(parser : PluginParser) extends IPresentationRe
   private val documentListener = new IDocumentListener {
     def documentAboutToBeChanged(event : DocumentEvent) {
       scalaDocument.edit(event.getOffset, event.getLength, event.getText)
+      if (damageEnd > 0) damageEnd = damageEnd - event.getLength + event.getText.length
     }
 
     def documentChanged(e : DocumentEvent) { }
@@ -38,7 +105,7 @@ class ScalaPresentationReconciler(parser : PluginParser) extends IPresentationRe
     if (document ne null) {
       document.addDocumentListener(documentListener)
       fViewer.addTextListener(textListener)
-      scalaDocument = new DefaultDocument
+      scalaDocument = new ScalaDocument
       scalaDocument.edit(0, 0, document.get)
       updatePresentation(document)
     }
@@ -46,10 +113,11 @@ class ScalaPresentationReconciler(parser : PluginParser) extends IPresentationRe
 
   private def updatePresentation(document : IDocument) {
     if (document ne null) {
-      val presentation = new TextPresentation(document.getPartition(0), 1000)
+      if (damageStart > 0 && damageEnd > damageStart) default(damageStart, damageEnd)
+      damageStart = -1
+      damageEnd = -1
       val input = new scala.ScalaInput(scalaDocument.first)
-      for (t <- parser.styleTokens(input)) t.addToPresentation(presentation)
-      fViewer.changeTextPresentation(presentation, false)
+      parser.compilationUnit(input)
     }
   }
   
